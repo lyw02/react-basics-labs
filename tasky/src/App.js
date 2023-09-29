48d9553aba2c97035fcbe533e84b4ff354845945
Yiwei Liu <2814824250@qq.com>
2023-09-29 23:07:58 +0800

    Week 3 Lab B done

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -4,17 +4,20 @@ import Task from './components/Task';
 import AddTaskForm from './components/Form';
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 
 function App() {
-  const [ taskState, setTaskState ] = useState({
+  const [taskState, setTaskState] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
+      { id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
       { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
-      { id: 3, title: "Tidy up", deadline: "Today", done: false}
+      { id: 3, title: "Tidy up", deadline: "Today", done: false }
     ]
   });
 
-  const [ formState, setFormState ] = useState({
+  const [formState, setFormState] = useState({
     title: "",
     description: "",
     deadline: ""
@@ -23,31 +26,31 @@ function App() {
   const doneHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
     tasks[taskIndex].done = !tasks[taskIndex].done;
-    setTaskState({tasks});
+    setTaskState({ tasks });
     console.log(`${taskIndex} ${tasks[taskIndex].done}`);
   }
 
   const deleteHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
     tasks.splice(taskIndex, 1);
-    setTaskState({tasks});
+    setTaskState({ tasks });
   }
 
   const formChangeHandler = (event) => {
-    let form = {...formState};
+    let form = { ...formState };
 
-    switch(event.target.name) {
+    switch (event.target.name) {
       case "title":
-          form.title = event.target.value;
-          break;
+        form.title = event.target.value;
+        break;
       case "description":
-          form.description = event.target.value;
-          break;
+        form.description = event.target.value;
+        break;
       case "deadline":
-          form.deadline = event.target.value;
-          break;
+        form.deadline = event.target.value;
+        break;
       default:
-          form = formState;
+        form = formState;
     }
     setFormState(form);
   }
@@ -56,32 +59,67 @@ function App() {
     event.preventDefault();
 
     const tasks = [...taskState.tasks];
-    const form = {...formState};
+    const form = { ...formState };
 
     form.id = uuidv4();
-    
+
     tasks.push(form);
-    setTaskState({tasks});
+    setTaskState({ tasks });
   }
 
   return (
     <div className="container">
-      <h1>Tasky</h1>
-      {taskState.tasks.map((task, index) => (
-        <Task
-          title={task.title}
-          description={task.description}
-          deadline={task.deadline}
-          key={task.id}
-          done={task.done}
-          markDone={() => doneHandler(index)}
-          deleteTask = {() => deleteHandler(index)}
-        />
-      ))}
-      <AddTaskForm 
-        submit={formSubmitHandler}
-        change={formChangeHandler}
-      />
+      {/* App Header */}
+      <Container component="main">
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          gutterBottom
+          sx={{
+            backgroundColor: 'gray',
+            textAlign: 'center',
+            color: 'white',
+            padding: '20px',
+            margin: '20px 0 40px 0',
+            borderRadius: '4px'
+          }}
+        >
+          Tasky
+        </Typography>
+      </Container>
+      {/* End App Header */}
+      {/* Task Card Grid */}
+      <Container maxWidth="md" component="header">
+        <Grid container spacing={5} alignItems="flex-top" justifyContent="center">
+          {taskState.tasks.map((task, index) => (
+                <Task 
+                title={task.title}
+                description={task.description}
+                deadline={task.deadline}
+                done={task.done}
+                key={task.id}
+                markDone = {() => doneHandler(index)}
+                deleteTask = {() => deleteHandler(index)}
+              />
+          ))}
+        </Grid>
+      </Container>
+      {/* End Task Card Grid */}
+      {/* Footer - Add Task Form */}
+      <Container
+        component="footer"
+        sx={{
+          borderTop: (theme) => `1px solid ${theme.palette.divider}`,
+          my: 6,
+          py: 6,
+        }}
+      >
+        <Grid container justifyContent="center">
+          <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
+        </Grid>
+      </Container>
+      {/* End Footer */}
     </div>
   );
 
